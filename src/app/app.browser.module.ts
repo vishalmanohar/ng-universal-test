6b0d364890a99a68f8af54a456ef515c509c73f6
Vishal Naik <vishalmanohar@gmail.com>
2021-06-09 16:41:57 +0530

    Reproducing ssr error with clr/angular + ng universal

---
+++ src/app/app.browser.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AppModule} from './app.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AppModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppBrowserModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
